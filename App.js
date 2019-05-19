30beed9189fba1ed54a703cfe39a4a86f2705699
Thiago Medeiros <si-thiago@hotmail.com>
2019-05-19 16:16:48 -0300

    type: feat, test
    Implemented button to reset the state
    Tests with button types behavior

--- App.js
+++ App.js
@@ -1,13 +1,70 @@
 import React from 'react'
-import { View } from 'react-native'
+import {
+	View,
+	Text,
+	StyleSheet,
+	TouchableHighlight,
+	TouchableNativeFeedback,
+	TouchableOpacity,
+	TouchableWithoutFeedback
+} from 'react-native'
 import AddEntry from './components/AddEntry'
+import Br from './components/globals/Br'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View>
-        <AddEntry />
-      </View>
-    )
-  }
+	handlePress = () => {
+		// alert('Hello!')
+	}
+	
+	render() {
+		return (
+			<View style={styles.container}>
+				<TouchableHighlight style={styles.btn} onPress={this.handlePress} underlayColor='#d4271b'>
+					<Text style={styles.btnText}>Touchable Highlight</Text>
+				</TouchableHighlight>
+				<Br/>
+				<TouchableOpacity style={styles.btn} onPress={this.handlePress} underlayColor='#d4271b'>
+					<Text style={styles.btnText}>Touchable Opacity</Text>
+				</TouchableOpacity>
+				<Br/>
+				<TouchableNativeFeedback backgroundstyle={styles.btn} onPress={this.handlePress}>
+					<View style={styles.btn}>
+						<Text style={styles.btnText}>Touchable Native Feedback</Text>
+					</View>
+				</TouchableNativeFeedback>
+				<Br/>
+				<TouchableWithoutFeedback
+					background={TouchableNativeFeedback.SelectableBackground()}
+					onPress={this.handlePress}>
+					<View style={styles.btn}>
+						<Text style={styles.btnText}>Touchable Without Feedback</Text>
+					</View>
+				</TouchableWithoutFeedback>
+			</View>
+		)
+	}
 }
+
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		marginLeft: 10,
+		marginRight: 10,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	btn: {
+		backgroundColor: '#E53224',
+		padding: 10,
+		paddingLeft: 50,
+		paddingRight: 50,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 5
+	},
+	btnText: {
+		color: '#fff'
+	}
+})
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,13 +1,103 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
+import Slider from './Slider'
+import Stepper from './Stepper'
+import DateHeader from './DateHeader'
+
+function SubmitBtn ({ onPress }) {
+	return (
+		<TouchableOpacity onPress={onPress}>
+			<Text>SUBMIT</Text>
+		</TouchableOpacity>
+	)
+}
 
 export default class AddEntry extends Component {
-  render() {
-    return (
-      <View>
-      	{getMetricMetaInfo('bike').getIcon()}
-      </View>
-    )
-  }
+	state = {
+		run: 0,
+		bike: 10,
+		swim: 0,
+		sleep: 0,
+		eat: 0,
+	}
+
+	increment = (metric) => {
+		const { max, sleep } = getMetricMetaInfo(metric)
+
+		this.setState((state) => {
+			return {
+				...state,
+				[metric]: count < 0 ? 0 : count
+			}
+		})
+	}
+	decrement = (metric) => {
+		const { max, sleep } = getMetricMetaInfo(metric)
+
+		this.setState((state) => {
+			const count = state[metric] - step
+
+			return {
+				...state,
+				[metric]: count > max ? max : count
+			}
+		})
+	}
+	slide = (metric, value) => {
+		this.setState(() =>  ({
+			[metric]: value
+		}))
+	}
+	submit = () => {
+		const key = timeToString()
+		const entry = this.state
+
+		// Udate Redux
+		this.setState(() => ({
+			run: 0,
+			bike: 0,
+			swim: 0,
+			sleep: 0,
+			eat: 0,
+		}))
+		// Navigate to Home
+		// Save to 'DB'
+		// Clear local notification
+	}
+
+
+	render() {
+		const metaInfo = getMetricMetaInfo()
+		return (
+			<View>
+				<DateHeader date={(new Date()).toLocaleDateString()}/>
+				<Text>{JSON.stringify(this.state)}</Text>
+				{Object.keys(metaInfo).map((key) => {
+					const { getIcon, type, ...rest } = metaInfo[key]
+					const value = this.state[key]
+
+					return (
+						<View key={key}>
+							{getIcon()}
+							{type === 'slider'
+								? <Slider
+									value={value}
+									onChange={(value) => this.slide(key, value)}
+									{...rest}
+								/>
+								: <Stepper
+									value={value}
+									onIncrement={() => this.increment(key)}
+									ondecrement={() => this.decrement(key)}
+									{...rest}
+								/>
+							}
+						</View>
+					)
+				})}
+				<SubmitBtn onPress={this.submit}/>
+			</View>
+		)
+	}
 }
